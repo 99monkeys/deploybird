a8a0bdf920dcd162f044b35ea0adcb45a08a81d8
Eugene Varlamov <varlamoved@gmail.com>
2015-05-26 14:09:32 +0300

    add capistrano deploy

---
+++ config/deploy.rb
@@ -0,0 +1,38 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+server 'ds04.inmyroom.ru', roles: %w{app web db},           user: 'strano' # ds09
+
+set :application, 'strano'
+set :repo_url,    'gitlab@git.improvemedia.ru:marvin/ru-improvemedia-meter.git'
+set :deploy_to,   "/opt/strano"
+
+set :linked_dirs, %w(tmp)
+
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :rvm_type,         :system
+set :rvm_custom_path,  '/opt/rvm'
+set :rvm_ruby_version, '2.1.2'
+set :rails_env,        :production
+
+
+
+namespace :deploy do
+  task :restart do
+    exec "if [ -f #{fetch(:unicorn_pid)} ]; then kill -USR2 `cat #{fetch(:unicorn_pid)}`; else cd #{fetch(:deploy_to)}/current && bundle exec unicorn -c #{fetch(:unicorn_conf)} -E #{fetch(:rails_env)} -D; fi"
+  end
+  task :start do
+    on roles(:app) do
+      within "#{fetch(:deploy_to)}/current" do
+        execute :bundle, 'exec bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"'
+      end
+    end
+  end
+  task :stop do
+    exec "if [ -f #{fetch(:unicorn_pid)} ]; then kill -QUIT `cat #{fetch(:unicorn_pid)}`; fi"
+  end
+end
+
+after 'deploy:publishing', 'unicorn:restart'
