e00a1b0c489ce9965422977ecf5a2a2611a392d8
Eugene Varlamov <varlamoved@gmail.com>
2015-09-28 13:09:13 +0300

    fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,17 +3,17 @@ lock '3.4.0'
 
 set :application, 'strano'
 set :repo_url,    'git@github.com:99monkeys/deploybird.git'
-set :deploy_to,   "/opt/strano"
+set :deploy_to,   "/srv/strano"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :rvm_type,         :system
-set :rvm_custom_path,  '/opt/rvm'
+set :rvm_type,         :user
 set :rvm_ruby_version, '2.1.2'
 set :rails_env,        :production
 
 set :linked_files,          ['config/database.yml', 'config/strano.yml', 'db/production.sqlite3']
 set :linked_dirs,           ['log', 'tmp', 'vendor/repos']
+set :bg_pid,                "#{shared_path}/tmp/pids/bg.pid"
 
 
 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
+server 'hw01.improvemedia.ru', roles: %w{app web db},           user: 'strano', port: '2224' # ds04
 
-server 'ds04.inmyroom.ru', roles: %w{app web db},           user: 'strano' # ds09
